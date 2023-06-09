b2df8849ba3c92dc5b0b6c9d08d793cf1bf6161a
hahmed <hahmed2@gmail.com>
2023-06-09 15:44:33 +0000

    Resolved merge conflicts in app

--- app.py
+++ app.py
@@ -3,18 +3,13 @@ from flask import Flask
 
 
 app = Flask(__name__)
-<<<<<<< HEAD
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-=======
-    
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
 @app.route("/test")
 def test():
-    return "<p>Tessting new route endpoint!</p>"
->>>>>>> 6ba4803d78e4a5dbe774d4cd6faadae2257c0a8c
+    return "<p>Testing new route endpoint!</p>"
+
