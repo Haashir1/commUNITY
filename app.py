6ade44120108bf814177e7778a53ea221a69cacf
Haashir1 <hahmed2@wihi.org>
2023-06-10 16:33:57 -0400

    updated flask

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
-
+app.debug = True
 @app.route('/')
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-@app.route("/test")
-def test():
-    return "<p>Testing new route endpoint!</p>"
-
+def index():
+    return render_template('index.html')
 
