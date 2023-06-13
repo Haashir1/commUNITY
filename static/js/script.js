a01f504e4c5033fa80bb5d60304681951dd594e4
Haashir1 <hahmed2@wihi.org>
2023-06-13 17:52:13 -0400

    h1

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,10 @@
 window.addEventListener('scroll', function() {
-    var scrollingImage = document.getElementById('scrolling-image');
-    var scrollAmount = window.pageYOffset;
-    scrollingImage.style.transform = 'translateY(' + (-scrollAmount/2) + 'px)';
-  });
-  
+  var scrollingImage = document.getElementById('header');
+  var scrollAmount = window.pageYOffset;
+
+  // Calculate the scroll ratio based on the window height
+  var scrollRatio = scrollAmount / window.innerHeight;
+
+  // Set the translateY value based on the scroll ratio
+  scrollingImage.style.transform = 'translateY(' + (-scrollRatio * 100) + '%)';
+});
