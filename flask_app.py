701eecaeb40044af76a0d3d271fc37c546ea93c8
Haashir1 <hahmed2@wihi.org>
2023-08-24 14:19:45 -0400

    s

--- flask_app.py
+++ flask_app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/help')
+@app.route('/volunteer')
 def help():
-    return render_template('Volunteer.html')
+    return render_template('help.html')
 
 
 @app.route('/donate')
